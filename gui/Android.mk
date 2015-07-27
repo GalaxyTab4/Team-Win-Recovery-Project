f47bafa9f7c4b04724cba533a3373ad10972c1bd
that <github@that.at>
2015-07-27 20:39:19 +0200

    gui: support new theme directory structure
    Change-Id: I8622da5ae321ba69a55b38e2f0b878b7ec149d02
    Signed-off-by: sub77 <sub77@ymail.com>

--- gui/Android.mk
+++ gui/Android.mk
@@ -83,7 +83,7 @@ ifneq ($(BOARD_SYSTEMIMAGE_PARTITION_SIZE),)
     LOCAL_CFLAGS += -DBOARD_SYSTEMIMAGE_PARTITION_SIZE=$(BOARD_SYSTEMIMAGE_PARTITION_SIZE)
 endif
 
-LOCAL_C_INCLUDES += bionic external/stlport/stlport system/core/libpixelflinger/include
+LOCAL_C_INCLUDES += bionic external/stlport/stlport
 LOCAL_CFLAGS += -DTWRES=\"$(TWRES_PATH)\"
 
 include $(BUILD_STATIC_LIBRARY)
@@ -158,41 +158,6 @@ else
         $(error stopping)
     endif
 
-    ifeq ($(TW_THEME_LANDSCAPE),)
-        # This converts the old DEVICE_RESOLUTION flag to the new TW_THEME flag
-        PORTRAIT_MDPI := 320x480 480x800 480x854 540x960
-        PORTRAIT_HDPI := 720x1280 800x1280 1080x1920 1200x1920 1440x2560 1600x2560
-        WATCH_MDPI := 240x240 280x280 320x320
-        LANDSCAPE_MDPI := 800x480 1024x600 1024x768
-        LANDSCAPE_HDPI := 1280x800 1920x1200 2560x1600
-        ifneq ($(filter $(LANDSCAPE_RESOLUTION), $(PORTRAIT_MDPI)),)
-            TW_THEME_LANDSCAPE := portrait_mdpi
-        else ifneq ($(filter $(LANDSCAPE_RESOLUTION), $(PORTRAIT_HDPI)),)
-            TW_THEME_LANDSCAPE := portrait_hdpi
-        else ifneq ($(filter $(LANDSCAPE_RESOLUTION), $(WATCH_MDPI)),)
-            TW_THEME_LANDSCAPE := watch_mdpi
-        else ifneq ($(filter $(LANDSCAPE_RESOLUTION), $(LANDSCAPE_MDPI)),)
-            TW_THEME := landscape_mdpi
-        else ifneq ($(filter $(LANDSCAPE_RESOLUTION), $(LANDSCAPE_HDPI)),)
-            TW_THEME_LANDSCAPE := landscape_hdpi
-        endif
-    endif
-    ifeq ($(TW_THEME_LANDSCAPE), portrait_mdpi)
-        TWRP_THEME_LOC_LANDSCAPE := $(commands_recovery_local_path)/gui/devices/480x800/res
-        TWRP_COMMON_XML_LANDSCAPE := cp -fr $(commands_recovery_local_path)/gui/devices/portrait/res/* $(TARGET_RECOVERY_ROOT_OUT)$(TWRES_PATH)landscape/
-    else ifeq ($(TW_THEME_LANDSCAPE), portrait_hdpi)
-        TWRP_THEME_LOC_LANDSCAPE := $(commands_recovery_local_path)/gui/devices/1080x1920/res
-        TWRP_COMMON_XML_LANDSCAPE := cp -fr $(commands_recovery_local_path)/gui/devices/portrait/res/* $(TARGET_RECOVERY_ROOT_OUT)$(TWRES_PATH)landscape/
-    else ifeq ($(TW_THEME_LANDSCAPE), watch_mdpi)
-        TWRP_THEME_LOC_LANDSCAPE := $(commands_recovery_local_path)/gui/devices/320x320/res
-        TWRP_COMMON_XML_LANDSCAPE := cp -fr $(commands_recovery_local_path)/gui/devices/watch/res/* $(TARGET_RECOVERY_ROOT_OUT)$(TWRES_PATH)landscape/
-    else ifeq ($(TW_THEME_LANDSCAPE), landscape_mdpi)
-        TWRP_THEME_LOC_LANDSCAPE := $(commands_recovery_local_path)/gui/devices/800x480/res
-        TWRP_COMMON_XML_LANDSCAPE := cp -fr $(commands_recovery_local_path)/gui/devices/landscape/res/* $(TARGET_RECOVERY_ROOT_OUT)$(TWRES_PATH)landscape/
-    else ifeq ($(TW_THEME_LANDSCAPE), landscape_hdpi)
-        TWRP_THEME_LOC_LANDSCAPE := $(commands_recovery_local_path)/gui/devices/1920x1200/res
-        TWRP_COMMON_XML_LANDSCAPE := cp -fr $(commands_recovery_local_path)/gui/devices/landscape/res/* $(TARGET_RECOVERY_ROOT_OUT)$(TWRES_PATH)landscape/
-    endif
 endif
 else
     TWRP_THEME_LOC := $(TW_CUSTOM_THEME)
