c4d83afbd0d1c7fe83c4282b8fb0fccf5b4c9074
Xing <xing@Xings-iMac.local>
2015-11-27 11:19:37 -0800

    do not include stlport headers unless link to it
    Change-Id: I8344d270ddb601694b5fef0fa7b22ea437030728
    Signed-off-by: sub77 <sub77@ymail.com>

--- mtp/Android.mk
+++ mtp/Android.mk
@@ -35,7 +35,7 @@ LOCAL_SRC_FILES = \
     node.cpp
 LOCAL_SHARED_LIBRARIES += libz libc libusbhost libstdc++ libdl libcutils libutils libaosprecovery
 
-ifeq (,$(filter $(PLATFORM_SDK_VERSION), 23))
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23; echo $$?),0)
     LOCAL_SHARED_LIBRARIES += libstlport
 else
     LOCAL_SHARED_LIBRARIES += libc++
